95e95e5d4e55e0bf60869c72eef76f4f26f99601
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2021-03-13 00:27:51 +0100

    NoteBook Audio cosas guays

--- curl.sh
+++ curl.sh
@@ -7,6 +7,6 @@ fi
 
 curl -X POST -u "apikey:${API_KEY}" \
 --header "Content-Type: audio/wav" \
---data-binary @sounds/asr-spanish/audios/d094e7e6-ae80-4fb2-8cc5-d09b9a7d71cb.wav \
+--data-binary @sounds/pulp-recortado.wav \
 https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/7311c985-e03f-4d80-80d5-fcace75363b9/v1/recognize?model=es-ES_BroadbandModel
 
