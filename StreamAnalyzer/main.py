b6bc0e6f0cb0e0e44e452f924abf79e2c089a36f
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2021-04-25 19:29:37 +0200

    Added liveplotter, tested with virtual cable and mic

--- StreamAnalyzer/main.py
+++ StreamAnalyzer/main.py
@@ -1,19 +1,26 @@
 import logging
 import sys
+import signal
 import numpy as np
 from numpy.core.fromnumeric import shape
 from numpy_ringbuffer import RingBuffer
 from numpy.core.numeric import full
 import parselmouth  # https://preaderselmouth.readthedocs.io/en/stable/
 import pyaudio
+import sounddevice as sa
 import os
 import time
 import pandas as pd
 from syllabe_nuclei import speech_rate
 from threading import Thread
-import matplotlib
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.interpolate import make_interp_spline, BSpline
+import pickle
+import redis
+from scipy.io import wavfile
+import queue
 
 """
 Installed on conda environment on W10
@@ -23,81 +30,80 @@ https://docs.conda.io/projects/conda/en/4.6.0/_downloads/52a95608c49671267e40c68
 
 
 np.set_printoptions(threshold=sys.maxsize)
-logging.basicConfig(level=logging.INFO,filename="data.log" ,format="%(message)s")
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 CHUNK = 1024  # Bytes of data to process
 RATE = 44100 // 2
-SECS = 20
-BUFFER_SIZE = RATE * SECS # BUFFER SIZE 
+SECS = 5
+BUFFER_SIZE = RATE * SECS  # BUFFER SIZE
+REDIS_PITCH = "PITCH"
+REDIS_INTENSITY = "INTENSITY"
+
 
 def callback(in_data, frame_count, time_info, status):
     data = np.frombuffer(in_data, dtype=np.int16)
-    logging.info(data)
+    global recorded_frames
+    recorded_frames.put(data)
     buffer.extend(data)
     return in_data, pyaudio.paContinue
 
 
-
-plt.style.use('ggplot')
-def live_plotter(x_vec,y1_data,line1,identifier='',pause_time=0.5):
-    if line1==None:
-        # this is the call to matplotlib that allows dynamic plotting
-        plt.ion()
-        fig = plt.figure(figsize=(13,6))
-        ax = fig.add_subplot(111)
-        # create a variable for the line so we can later update it
-        line1, = ax.plot(x_vec,y1_data,alpha=0.8, linewidth=3)        
-        #update plot label/title
-        plt.ylabel('Y Label')
-        plt.title('Title: {}'.format(identifier))
-        plt.show()
-    
-    # after the figure, axis, and line are created, we only need to update the y-data
-    line1.set_ydata(y1_data)
-    # adjust limits if new data goes beyond bounds
-    if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
-        plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
-    # this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
-    plt.pause(pause_time)
-    
-    # return line so we can update it again in the next iteration
-    return line1
+def signal_handler(signal=None, frame=None):
+    print("\nprogram exiting gracefully")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    output = np.array(recorded_frames.queue, dtype=np.int16).flatten()
+    wavfile.write("example.wav", RATE, output)
+    sys.exit(0)
 
 
-def output_info(snd, line1):
-    if snd.duration > 0.064:
-        #print(speech_rate(sound=snd))
-        intensity = snd.to_intensity()
-        pitch = snd.to_pitch()
-        print(np.mean(intensity))
-        print(np.mean(pitch.selected_array['frequency']))
-        line1 = live_plotter(intensity.xs(), intensity.values.T, line1)
-        return line1
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
 
-
-
-
-fps = 60 # Number of frames per seconds
+fps = 30  # Number of frames per seconds
 time_elapsed = 0
 start_time = 0
 last_update = 0
 
+r = redis.Redis()
+r.flushall()
 p = pyaudio.PyAudio()
-stream = p.open(
-    format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK, stream_callback=callback
-)
 buffer = RingBuffer(capacity=(BUFFER_SIZE), dtype=np.int16)
+recorded_frames = queue.Queue()
+
+for device_index in range(p.get_device_count()):
+    device_info = p.get_device_info_by_index(device_index)
+    if device_info["maxInputChannels"] > 0:
+        print(device_info)
+
+stream = p.open(
+    format=pyaudio.paInt16,
+    channels=1,
+    rate=RATE,
+    input=True,
+    output=False,
+    frames_per_buffer=CHUNK,
+    stream_callback=callback,
+    input_device_index=1,
+)
+time.sleep(1)
+
 start_time = time.time()
-line1 = []
-while time_elapsed <= 30:  # go for a few seconds
-    if (time.time() - last_update) > (1./fps):
+while time_elapsed <= 40:  # go for a few seconds
+    if (time.time() - last_update) > (1.0 / fps):
         last_update = time.time()
         ini = time.time()
         buff = np.array(buffer)
         snd = parselmouth.Sound(buff, sampling_frequency=RATE)
         print(snd.duration)
-        line1 = output_info(snd, line1)
+        intensity = snd.to_intensity(50)
+        pitch = snd.to_pitch()
+        pitch_values = pitch.selected_array['frequency']
         ### Time Elapsed ###
+        r.xadd(REDIS_INTENSITY, {"intensity": intensity.values.T.tobytes(), "seconds": intensity.xs().tobytes()})
+        r.xadd(REDIS_PITCH, {"pitch": pitch_values.tobytes(), "seconds": pitch.xs().tobytes()})
+        # time.sleep(1)
         time_elapsed = time.time() - start_time
-        print("Time elapsed:", time_elapsed)
-
+        # print("Time elapsed:", time_elapsed)
+signal_handler()
