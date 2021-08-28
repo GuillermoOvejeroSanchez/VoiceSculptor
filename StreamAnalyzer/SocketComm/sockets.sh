b1da1bd817d0d092811661adab2ff477e3956228
Guillermo Ovejero <guilleov@ucm.es>
2021-08-28 18:05:09 +0200

    change script to run sockets

--- StreamAnalyzer/SocketComm/sockets.sh
+++ StreamAnalyzer/SocketComm/sockets.sh
@@ -3,9 +3,8 @@
 python3 dash_try.py &
 python3 main_client.py &
 
-read -r
+read
 
 echo "Stopping Dash Server and Voice Client..."
 
-pkill -f dash_try.py
-pkill -f main_client.py
+pkill -f main_client && pkill -f dash_try
