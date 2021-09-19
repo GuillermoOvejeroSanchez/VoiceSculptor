ff14206b4977f5ea01c966f18744bd04009ad5c3
Guillermo Ovejero (Wilson) <guilleov@ucm.es>
2021-09-19 20:41:38 +0200

    Update get_input_devices.py

--- utils/get_input_devices.py
+++ utils/get_input_devices.py
@@ -1,8 +1,6 @@
 import pyaudio
 
 p = pyaudio.PyAudio()
-
-# Voicemeteer Potato/Banana
 for device_index in range(p.get_device_count()):
     device_info = p.get_device_info_by_index(device_index)
     if device_info["maxInputChannels"] > 0:
